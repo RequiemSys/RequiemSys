2689138b99f188bff17d03ca991899bc148aafb1
Luizdluna <luna.luiz3010@gmail.com>
2026-04-13 23:44:59 -0300

    adjustments: login buttons

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Registro } from './pages/registro/registro';
 import { LoginDois } from './pages/login-dois/login-dois';
 
 export const routes: Routes = [
+    {path: '', component: Login}, // rota padrão
     {path: 'login', component: Login}, // rota do login
     {path: 'registro', component: Registro }, // rota do registro
     {path: 'login-dois', component: LoginDois }, // rota do registro
--- apps/frontend/src/app/pages/login/login.ts
+++ apps/frontend/src/app/pages/login/login.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [Login],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private readonly router = inject(Router);
 
+  irParaLogin(): void {
+    void this.router.navigateByUrl('/login-dois');
+  }
+
+  irParaRegistro(): void {
+    void this.router.navigateByUrl('/registro');
+  }
 }
