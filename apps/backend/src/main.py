8d7991967941a7de7f36ef51557d1d5787434c0e
Lucas Alves Brito <lucas.alves.brito@g.globo>
2026-04-14 01:10:23 -0300

    "add api router prefix"

--- apps/backend/src/main.py
+++ apps/backend/src/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from src.api import routes
+
 app = FastAPI()
+
+app.include_router(routes.api_router, prefix="/api/v1")
 
 origins = ["http://localhost:4200"]
 
