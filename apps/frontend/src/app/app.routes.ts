6778f05257b8d0a04088d94450254e20cacc3ea6
Lucas Alves Brito <lucas.alves.brito@g.globo>
2026-04-27 02:19:22 -0300

    as

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Registro } from './pages/registro/registro';
-import { LoginDois } from './pages/login-dois/login-dois';
 import { BaseLayout } from './pages/base-layout/base-layout';
 
 export const routes: Routes = [
     {path: '', component: Login},
-    {path: 'login-dois', component: LoginDois },
     {path: 'base',
     component: BaseLayout,
     children:[  
