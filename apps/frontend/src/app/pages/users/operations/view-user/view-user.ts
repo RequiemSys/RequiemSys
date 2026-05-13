3cc3ea216514ed2a015ecdb853e89d49f3d76b0a
Yasmin <yasminmarques116@gmail.com>
2026-05-13 13:12:14 -0300

    feat: create view-user interface

--- apps/frontend/src/app/pages/users/operations/view-user/view-user.ts
+++ apps/frontend/src/app/pages/users/operations/view-user/view-user.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ViewUserModalComponent } from './view-user-modal/view-user-modal';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-view-user',
   imports: [
     CommonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatIconModule
   ],
   templateUrl: './view-user.html',
   styleUrl: './view-user.css',
@@ -17,11 +19,15 @@ export class ViewUserComponent {
   users = [
     {
       id: 1,
-      name: 'Rodrigo Silva'
+      name: 'Rodrigo Silva',
+      email: 'rodrigo.silva@gmail.com',
+      acess: 'Funcionário'
     },
     {
       id: 2,
-      name: 'Ana Rodrigues'
+      name: 'Ana Rodrigues',
+      email: 'ana.rodrigues@gmail.com',
+      acess: 'Administrador'
     }
   ]
 
