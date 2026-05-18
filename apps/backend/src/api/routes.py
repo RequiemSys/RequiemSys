d6013c0743b040baba8f782a3d8def3e2a7d6249
Guilherme F <88292977+Guiisantoos@users.noreply.github.com>
2026-05-18 22:47:28 +0000

    feat: adiciona módulo de jazigos, entrega 5 finalizado

--- apps/backend/src/api/routes.py
+++ apps/backend/src/api/routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 from modules.responsible.routes import responsible_route
 from modules.users.routes import user_route
 from modules.falecidos.routes import falecido_route
+from modules.jazigos.routes import jazigo_route
+
 #incluir todas as rotas aqui
 
 api_router = APIRouter()
@@ -9,3 +11,4 @@ api_router = APIRouter()
 api_router.include_router(user_route.router)
 api_router.include_router(falecido_route.router)
 api_router.include_router(responsible_route.router)
+api_router.include_router(jazigo_route.router)
