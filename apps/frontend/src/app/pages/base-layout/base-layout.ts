8be59c4d47c2d7ede0daee1e9cdbe0404e01c3f6
Yasmin <yasminmarques116@gmail.com>
2026-06-03 09:23:16 -0300

    style: update sidebar icons

--- apps/frontend/src/app/pages/base-layout/base-layout.ts
+++ apps/frontend/src/app/pages/base-layout/base-layout.ts
@@ -5,6 +5,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'base-layout',
@@ -23,4 +25,15 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class BaseLayout {
   private readonly router = inject(Router);
+  private readonly iconRegistry = inject(MatIconRegistry);
+  private readonly sanitizer = inject(DomSanitizer);
+
+  constructor() {
+    this.iconRegistry.addSvgIcon(
+      'tombstone',
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        'tombstone-svgrepo.svg'
+      )
+    );
+  }
 }
