5c436bf3b4367d514a18443cb2782a09a6f353d6
Yasmin <yasminmarques116@gmail.com>
2026-06-04 12:05:57 -0300

    style: add colored borders and icons to cards

--- apps/frontend/src/app/pages/home/home.ts
+++ apps/frontend/src/app/pages/home/home.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatIconModule,
+    CommonModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class HomeComponent {
 
+  atividadesRecentes = [
+    { descricao: 'Falecido Teste cadastrado', horario: 'Hoje 11:39' }
+  ];
+
+  notificacoesRecentes = [
+    { descricao: 'Aviso de Teste enviado', horario: 'Hoje 16:58' }
+  ];
+
+  atividadesUsuarios = [
+    { descricao: 'Usuário Teste logou', horario: 'Hoje 09:00' }
+  ];
+
+  constructor (
+    private MatIconRegistry: MatIconRegistry,
+    private DomSanitizer: DomSanitizer
+  ) {
+    this.MatIconRegistry.addSvgIcon(
+      'tombstone',
+      this.DomSanitizer.bypassSecurityTrustResourceUrl('tombstone-svgrepo.svg')
+    );
+  }
 }
